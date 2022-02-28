8f64ff2b27461a3b306b4e4238bd85ed4b9eac6d
Pauline Isabela Conti <pauline.conti@epfl.ch>
2022-02-28 18:54:53 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ then
     export ML100Ku2test=hdfs://iccluster028.iccluster.epfl.ch:8020/cs449/data/ml-100k/u2.test;
     export ML100Kudata=hdfs://iccluster028.iccluster.epfl.ch:8020/cs449/data/ml-100k/u.data;
     export ML25Mr2train=hdfs://iccluster028.iccluster.epfl.ch:8020/cs449/data/ml-25m/r2.train;
-    export ML25Mr2test=hdfs:hdfs://iccluster028.iccluster.epfl.ch:8020/cs449/data/ml-25m/r2.test;
+    export ML25Mr2test=hdfs://iccluster028.iccluster.epfl.ch:8020/cs449/data/ml-25m/r2.test;
     export SPARKMASTER='yarn'
 else 
     export ML100Ku2base=data/ml-100k/u2.base;
